fe9ae75e1222d66d7a4b04db36d51904bfbc03c2
Mehmet Tarhan <hi@memtarhan.com>
2025-05-20 15:35:43 +0100

    <Flutter>: Recipes app

--- flutter-apprentice/recipes/lib/recipe_detail.dart
+++ flutter-apprentice/recipes/lib/recipe_detail.dart
@@ -14,7 +14,7 @@ class RecipeDetail extends StatefulWidget {
 }
 
 class _RecipeDetailState extends State<RecipeDetail> {
-  // TODO: Add _sliderVal here
+  int _sliderVal = 1;
   @override
   Widget build(BuildContext context) {
     // 1
@@ -35,8 +35,36 @@ class _RecipeDetailState extends State<RecipeDetail> {
             const SizedBox(height: 4),
             // 6
             Text(widget.recipe.label, style: const TextStyle(fontSize: 18)),
-            // TODO: Add Expanded
-            // TODO: Add Slider() here
+            // 7
+            Expanded(
+              // 8
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  // 9
+                  // TODO: Add ingredient.quantity
+                  return Text(
+                      '${ingredient.quantity * _sliderVal} ${ingredient.measure} ${ingredient
+                          .name}');
+                },
+              ),
+            ),
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 9,
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              value: _sliderVal.toDouble(),
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
           ],
         ),
       ),

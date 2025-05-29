de9ab9243bcb5e808ff589582a14d89e1b231cb9
Mehmet Tarhan <hi@memtarhan.com>
2025-05-29 20:32:28 +0100

    <Flutter>: Networking, Persistence and State starter project

--- flutter-apprentice/recipes/lib/main.dart
+++ flutter-apprentice/recipes/lib/main.dart
@@ -1,135 +1,85 @@
+import 'package:colorize_lumberdash/colorize_lumberdash.dart';
+import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lumberdash/lumberdash.dart';
 
-import 'recipe.dart';
-import 'recipe_detail.dart';
+import 'ui/main_screen.dart';
+import 'ui/theme/theme.dart';
+import 'utils.dart';
+import 'package:logging/logging.dart' as system_log;
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  _setupLogging();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+  if (isDesktop()) {
+    await DesktopWindow.setWindowSize(const Size(600, 600));
+    await DesktopWindow.setMinWindowSize(const Size(260, 600));
+  }
+  // TODO Add Shared Preferences
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+void _setupLogging() {
+  putLumberdashToWork(withClients: [
+    ColorizeLumberdash(),
+  ]);
+  system_log.Logger.root.level = system_log.Level.ALL;
+  system_log.Logger.root.onRecord.listen((rec) {
+    debugPrint('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    final ThemeData theme = ThemeData();
-
-    return MaterialApp(
-      title: 'Recipes',
-      theme: theme.copyWith(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-      ),
-      home: const MyHomePage(title: 'Recipes'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAppState extends State<MyApp> {
+  ThemeMode currentMode = ThemeMode.light;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // 1
-    return Scaffold(
-      // 2
-      appBar: AppBar(title: Text(widget.title)),
-      // 3
-      body: SafeArea(
-        // 4
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: ListView.builder(
-            // 5
-            itemCount: Recipe.samples.length,
-            // 6
-            itemBuilder: (BuildContext context, int index) {
-              // 7
-              return GestureDetector(
-                // 8
-                onTap: () {
-                  // 9
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        // 10
-                        return RecipeDetail(recipe: Recipe.samples[index]);
-                      },
-                    ),
-                  );
-                },
-                // 11
-                child: buildRecipeCard(Recipe.samples[index]),
-              );
+    return PlatformMenuBar(
+      menus: [
+        PlatformMenu(label: 'File', menus: [
+          PlatformMenuItem(
+              label: 'Dark Mode',
+              onSelected: () {
+                setState(() {
+                  currentMode = ThemeMode.dark;
+                });
+              }),
+          PlatformMenuItem(
+              label: 'Light Mode',
+              onSelected: () {
+                setState(() {
+                  currentMode = ThemeMode.light;
+                });
+              }),
+          PlatformMenuItem(
+            label: 'Quit',
+            onSelected: () {
+              setState(() {
+                SystemNavigator.pop();
+              });
             },
+            shortcut:
+            const SingleActivator(LogicalKeyboardKey.keyQ, meta: true),
           ),
-        ),
-      ),
-    );
-  }
-
-  // Widget buildRecipeCard(Recipe recipe) {
-  //   return Card(
-  //     child: buildRecipeCardColumn(recipe)
-  //   );
-  // }
-  //
-  // Column buildRecipeCardColumn(Recipe recipe) {
-  //   return Column(
-  //     children: <Widget>[
-  //       Image(image: AssetImage(recipe.imageUrl)),
-  //       Text(recipe.label),
-  //     ],
-  //   );
-  // }
-
-  Widget buildRecipeCard(Recipe recipe) {
-    return Card(
-      // 1
-      elevation: 2.0,
-      // 2
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-      // 3
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        // 4
-        child: Column(
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: Image(image: AssetImage(recipe.imageUrl)),
-            ),
-            // Image(image: AssetImage(recipe.imageUrl)),
-            // 5
-            const SizedBox(height: 14.0),
-            // 6
-            Text(
-              recipe.label,
-              style: const TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w700,
-                fontFamily: 'Palatino',
-              ),
-            ),
-          ],
-        ),
+        ])
+      ],
+      child: MaterialApp(
+        title: 'Recipes',
+        debugShowCheckedModeBanner: false,
+        themeMode: currentMode,
+        theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+        darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+        home: const MainScreen(),
       ),
     );
   }

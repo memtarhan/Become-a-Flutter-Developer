b76d8f9067004a06077def6e0f6c79cb563072ea
Mehmet Tarhan <hi@memtarhan.com>
2025-06-04 13:22:44 +0100

    <Flutter Cookbook>: using widgets

---
+++ flutter-cookbook/chapter_4/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'immutable_widget.dart';
+
+// void main() => runApp(const StaticApp());
+void main() {
+  runApp(const StaticApp());
+}
+
+class StaticApp extends StatelessWidget {
+  const StaticApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const ImmutableWidget(),
+    );
+  }
+}

2b958fb89d8aae112753e9231f06e0f27ca605cb
Mehmet Tarhan <hi@memtarhan.com>
2025-06-13 08:20:41 +0100

    <Flutter Cookbook>: more on Widgets

---
+++ flutter-projects/login_app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/memtarhan/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/memtarhan/Developer/Become-a-Flutter-Developer/flutter-projects/login_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/memtarhan/Developer/Become-a-Flutter-Developer/flutter-projects/login_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_CLI_BUILD_MODE=debug"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzMuMC0xLjAucHJlLjM2Ng==,RkxVVFRFUl9DSEFOTkVMPW1hc3Rlcg==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZDI4ZjNhZmYxYg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZDI4ZjNhZmYxYg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjAgKGJ1aWxkIDMuOS4wLTE3Mi4wLmRldik="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/memtarhan/Developer/Become-a-Flutter-Developer/flutter-projects/login_app/.dart_tool/package_config.json"

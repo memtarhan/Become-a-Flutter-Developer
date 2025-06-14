ffff33eba74ce0fe85b851d41d0ba674775ff885
Mehmet Tarhan <hi@memtarhan.com>
2025-06-14 07:47:39 +0100

    <Flutter Cookbook>: Buttons

--- flutter-cookbook/stopwatch/lib/stopwatch.dart
+++ flutter-cookbook/stopwatch/lib/stopwatch.dart
@@ -10,6 +10,7 @@ class Stopwatch extends StatefulWidget {
 }
 
 class StopwatchState extends State<Stopwatch> {
+  bool isTicking = false;
   int seconds = 0;
   late Timer timer;
 
@@ -33,15 +34,63 @@ class StopwatchState extends State<Stopwatch> {
     }
   }
 
+  void _startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), onTrick);
+    setState(() {
+      seconds = 0;
+      isTicking = true;
+    });
+  }
+
+  void _stopTimer() {
+    setState(() {
+      isTicking = false;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Stopwatch')),
-      body: Center(
-        child: Text(
-          '$seconds ${_secondsText()}',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$seconds ${_secondsText()}',
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .headlineSmall,
+            ),
+            const SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(padding: EdgeInsets.all(12), child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Colors.green),
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        Colors.white),
+                  ),
+                  onPressed: isTicking ? null : _startTimer,
+                  child: const Text('Start'),
+                ),),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Colors.red),
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                          Colors.white),
+                    ),
+                    onPressed: isTicking ? _stopTimer : null,
+                    child: const Text('Stop'),
+                  ),
+                )
+              ],
+            )
+          ]
       ),
     );
   }
